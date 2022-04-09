068e7c35efc524f3b9427e6cccf87d5c483d88d2
Ashfak Uddin <ashmanpapet@gmail.com>
2022-04-09 11:08:08 -0400

    made a basic login page

--- client2/src/index.js
+++ client2/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import Message from './message.jsx';
-import List from './list.jsx';
-
+//import List from './list.jsx';
+import Login from './login.jsx';
 class Layout extends React.Component
 {
 	render()
 	{
 		return <div>
-			<List />
 		</div>
 	}
 }
@@ -16,4 +14,4 @@ const container = document.getElementById('root');
 
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
-root.render(<Layout />);
+root.render(<Login />);
