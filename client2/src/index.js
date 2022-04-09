bdbc46f733c60f51ac80bb60b0c3f91ca1c9fb6c
Ashfak Uddin <ashmanpapet@gmail.com>
2022-04-09 17:10:38 -0400

    Idkkkk

--- client2/src/index.js
+++ client2/src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 //import List from './list.jsx';
 import Login from './login.jsx';
-class Layout extends React.Component
-{
-	render()
-	{
-		return <div>
-		</div>
-	}
-}
 const container = document.getElementById('root');
 
 // Create a root.
