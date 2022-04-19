bd586f35432dc58010256338fb856f249ce8b101
Ashfak Uddin <ashmanpapet@gmail.com>
2022-04-18 20:44:09 -0400

    Kene

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-const port = /*process.env.PORT ||*/ 5000;
+const port = process.env.PORT || 5000;
 
 const uri = "mongodb+srv://ashfaku:Ashman123@cluster0.w7fyh.mongodb.net/Cluster0?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
